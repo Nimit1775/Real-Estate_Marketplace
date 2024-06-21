3970211af65a194628ef83f4086c652568afa3d2
nimit sodhani <nimitsodhani1@gmail.com>
2024-06-22 02:54:43 +0530

    sign in api route completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcrypt';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 export const signup =  async ( req , res , next  )=>{
 
     const { username , email , password } =  req.body;
@@ -20,5 +22,28 @@ export const signup =  async ( req , res , next  )=>{
             next(error); 
 
         }
+    };
+
+    export const signin  = async ( req , res , next )=>{
+        const { email , password } = req.body;
+        try {
+            const validuser = await User.findOne({ email });
+            if (!validuser) {
+                return next(errorHandler(404, "User not found"));
+            }
+            const validPassword = bcryptjs.compareSync(password, validuser.password);
+            if (!validPassword) {
+                return next(errorHandler(401, "wrong credentials"));
+            }
+            const token = jwt.sign({ id : validuser._id } , process.env.JWT_SECRET );
+            const {password : pass, ...rest}=validuser._doc;
+            res.cookie('access_token' , token , { httpOnly : true }).status(200).json(rest);
+
+            
+
+    }
+    catch (error) {
+        next(error); 
+    }
     };
    
