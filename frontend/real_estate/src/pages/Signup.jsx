a08cde747935647661025ac167e2d731f3455c0c
nimit sodhani <nimitsodhani1@gmail.com>
2024-06-21 19:21:16 +0530

    auth ui started

--- frontend/real_estate/src/pages/Signup.jsx
+++ frontend/real_estate/src/pages/Signup.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <div>
+      <h1 className='text-3xl text-center font-semibold my-7'>
+        Signup
+      </h1>
+      <form >
+        <input type="text" placeholder='username' 
+        className='border p-3 rounded-lg ' id=' username '/> 
+      </form>
+    </div>
   )
 }
