8b0f8823b68eead50a73ad64bfd7e01b2398a0b8
nimit sodhani <nimitsodhani1@gmail.com>
2024-06-21 16:48:33 +0530

    added headers

--- frontend/real_estate/src/App.jsx
+++ frontend/real_estate/src/App.jsx
@@ -5,8 +5,10 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import Home from './pages/Home'
 import Signin from './pages/Signin'
+import Header from './components/Header'
 export default function App() {
   return  <BrowserRouter>
+  <Header />
   <Routes>
     <Route path="/" element={<Home/>} />
     <Route path="/sign-in" element={<Signin/>} />
