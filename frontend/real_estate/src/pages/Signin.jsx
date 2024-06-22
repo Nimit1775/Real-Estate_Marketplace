25b81cfbfca4d361f5e172af0b1d9691fc66a036
nimit sodhani <nimitsodhani1@gmail.com>
2024-06-22 20:15:29 +0530

    oAuth error

--- frontend/real_estate/src/pages/Signin.jsx
+++ frontend/real_estate/src/pages/Signin.jsx
@@ -70,7 +70,7 @@ export default function Signin() {
         <button
           disabled={loading}
           className={`bg-slate-800 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80`}         
-        >
+          >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
         <Oauth />
