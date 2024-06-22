5e57fc534a3ce2e0bb6cbc269926c23dfe21ad67
nimit sodhani <nimitsodhani1@gmail.com>
2024-06-22 15:14:00 +0530

    added redux persist

--- frontend/real_estate/src/main.jsx
+++ frontend/real_estate/src/main.jsx
@@ -4,8 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
+import { persistor } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider  store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
   </Provider>,
 )
