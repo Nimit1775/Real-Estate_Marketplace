20518a9c9cbb9a41cd7db647b0fb87a9518e9221
nimit sodhani <nimitsodhani1@gmail.com>
2024-06-24 04:00:25 +0530

    building the image upload functionality

--- frontend/real_estate/src/pages/Profile.jsx
+++ frontend/real_estate/src/pages/Profile.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState  , useEffect} from 'react';
 import { useSelector } from "react-redux";
-
+import { useRef } from 'react';
+import { getStorage, ref, uploadBytes, uploadBytesResumable} from 'firebase/storage';
+import { app } from '../firebase';
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
 
+  console.log(file);
+
+  useEffect(()=>{
+    if(file){
+      handleFileUpload(file);   
+    }
+    } , [file]);
+
+  const handleFileUpload =(file)=>{
+    const storage = getStorage(app);
+    const fileName =   new Date().getTime()+  file.name;
+    const storageRef =  ref(storage , fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file); 
+
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+      }
+    )
+
+  }
   return (
     <div className='p-6 max-w-lg mx-auto bg-white rounded-lg shadow-lg'>
       <h1 className='text-4xl font-bold text-center my-7 text-gray-800'>
@@ -11,7 +37,8 @@ export default function Profile() {
       </h1>
       <form className='flex flex-col gap-6'>
         <div className='flex justify-center'>
-          <img
+          <input onChange={(e)=>setFile(e.target.files[0])}  type="file" ref={fileRef}  hidden accept='image/*' />
+          <img onClick={()=>fileRef.current.click()}
             className='rounded-full h-28 w-28 object-cover cursor-pointer transition-transform hover:scale-105 shadow-md'
             src={currentUser.avatar}
             alt='Profile'
